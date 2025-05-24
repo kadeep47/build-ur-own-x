769799610fd038f28e5ef66d43cd62202dc981cc
Kumar Akashdeep <kadeep47@gmail.com>
2025-05-24 18:55:14 +0530

    custom struct for price

--- rust/trade_engine/src/main.rs
+++ rust/trade_engine/src/main.rs
@@ -1,45 +1,47 @@
-enum BidOrAsk{
+#[derive(Debug)]
+struct Price {
+    integral:   u64,
+    fractional: u64,
+    scalar:     u64,
+}
+
+impl Price {
+    fn new(price: f64) -> Price {
+        let scalar: u64 = 100_000;                   // how many decimal places
+        let integral      = price as u64;            // truncate to integer
+        let fractional    = (price.fract() * scalar as f64) as u64;
+        
+        Price {
+            integral,
+            fractional,
+            scalar,
+        }
+    }
+}
+
+enum BidOrAsk {
     Bid,
     Ask,
 }
 
-struct Order{
-    size : f64,
-    bid_or_ask : BidOrAsk
-
+struct Order {
+    size:      f64,
+    bid_or_ask: BidOrAsk,
 }
 
-struct Price{
-    integral : u64,
-    fractional : u64,
-    scalar : u64,
-}
-
-impl Price{
-    fn new(price: f64) -> Price{
-        let scalar = 1000;
-        let integral = price as u64 
-        Price{
-            integral = ,
-            fractional = ,
-            scalar = ,
-        }
+impl Order {
+    fn new(bid_or_ask: BidOrAsk, size: f64) -> Order {
+        Order { bid_or_ask, size }
     }
 }
 
-struct Limit{
-    price: Price,
-    orders: Vec<Order>
-}
-
-impl Order{
-    fn new(bid_or_ask:BidOrAsk, size:f64) -> Order{
-        Order{
-            bid_or_ask,size
-        }
-    }
+struct Limit {
+    price:  Price,
+    orders: Vec<Order>,
 }
 
 fn main() {
+    let price = Price::new(50.77);
+    println!("Price broken down: {:?}", price);
     println!("Hello, world!");
 }
